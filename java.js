dd5db21c71db791a97fdddaa1f5bfc9886ac90ea
Your Name <you@example.com>
2021-01-27 14:43:52 +0600

    added new exercises

---
+++ java.js
@@ -0,0 +1,31 @@
+
+ function printLine(text){
+     document.write("<br>" + text) 
+ }
+// function sayHello(firstName, LastName){
+//     printLine("Hello " + firstName + "  " +  LastName + " !<br>");
+// }
+
+// function sayBye(firstName, LastName){
+//     printLine("Bye " + firstName + "  " +  LastName + " !<br>");
+// }
+// let name = "Keremet";
+// function sayHowdy(){
+//     printLine("Howdy " + name + "!<br>");
+// }
+
+// sayHello("Keremet", "Kerimova");
+// sayBye("AIdanek", "Kerimova");
+// sayHowdy();
+
+// Exercise #1
+
+
+function printMax(a, b) {
+    if (a < b){
+    printLine(b);
+    }
+    else {
+    printLine(a);
+}
+}
